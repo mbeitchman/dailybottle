337571ef0f3017127233f4e909bcebfc42517bc1
Marc Beitchman <marcb@spotify.com>
2014-08-12 16:12:35 +0200

    fix typo in view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Hellow world. Daily bottle is awesome!"));
+        return ok(index.render("Hello world. Daily bottle is awesome!"));
     }
 
 }
